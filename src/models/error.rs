c93e8c6b343da16ddac395b9129c22c3e43e844c
onotaizee@gmail.com <onotaizee@gmail.com>
2023-09-28 16:20:38 +0100

    feat: error handling

--- src/models/error.rs
+++ src/models/error.rs
@@ -4,7 +4,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-	LoginFail,
+	LoginFail, // Due to invalid credentials
 
 	// -- Auth errors.
 	AuthFailNoAuthTokenCookie,
@@ -33,6 +33,16 @@ impl IntoResponse for Error {
 	fn into_response(self) -> axum::response::Response {
 		println!("->> {:<12} - {self:?}", "INTO_RES");
 
-		(StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+		match self {
+			// Unauthenticated
+			Error::AuthFailCookieExpired => (StatusCode::UNAUTHORIZED, "AUTH_FAILED_COOKIE_EXPIRED").into_response(),
+			Error::AuthFailNoAuthTokenCookie => (StatusCode::UNAUTHORIZED, "AUTH_FAILED_NO_AUTH_TOKEN_COOKIE").into_response(),
+			Error::InvalidJwt => (StatusCode::UNAUTHORIZED, "INVALID_JWT").into_response(),
+
+
+			Error::LoginFail => (StatusCode::BAD_REQUEST, "LOGIN_FAILED_INVALID_CREDENTIALS").into_response(),
+			Error::InternalServerError => (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response(),
+			_ => (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+		}
 	}
 }
